d98df5a8bad26a6b4d7ef43925d1daeb86515da2
canpok1 <6isstrong@gmail.com>
2022-06-24 14:17:42 +0000

    TrainingBatchの定期実行機能を実装

--- training-batch/src/config.rs
+++ training-batch/src/config.rs
@@ -8,4 +8,15 @@ pub struct Config {
     pub db_name: String,
     pub db_user_name: String,
     pub db_password: String,
+
+    // 共通設定
+    pub forecast_input_size: usize,
+    pub forecast_offset_minutes: usize,
+    pub currency_pair: String,
+
+    // バッチ関連
+    pub cron_schedule: String,
+    pub training_count: usize,
+    pub training_data_required_count: usize,
+    pub forecast_model_no: i32,
 }
--- training-batch/src/main.rs
+++ training-batch/src/main.rs
@@ -7,6 +7,7 @@ use common_lib::{
     },
     domain::model::ForecastModel
 };
+use job_scheduler::{JobScheduler, Job};
 use log::{error, info, warn, debug};
 use smartcore::{
     linalg::naive::dense_matrix::DenseMatrix,
@@ -52,19 +53,34 @@ fn main() {
         }
     }
 
-    info!("start training");
-    match training(&mysql_cli) {
-        Ok(_) => {
-            info!("finished training");
-        }
-        Err(err) => {
-            error!("failed to training, error:{}", err);
-        }
+    if let Err(err) = start_scheduler(&config, &mysql_cli) {
+        error!("failed to start scheduler, error: {}", err);
     }
 }
 
-fn training(mysql_cli: &DefaultClient) -> MyResult<()> {
-    let test_data_size = 50;
+fn start_scheduler(config: &config::Config, mysql_cli: &mysql::client::DefaultClient) -> MyResult<()> {
+    let mut sched = JobScheduler::new();
+
+    info!("set cron schedule: {}", &config.cron_schedule);
+    sched.add(Job::new(config.cron_schedule.parse()?, || {
+        info!("start training");
+        match training(config, mysql_cli) {
+            Ok(_) => {
+                info!("finished training");
+            }
+            Err(err) => {
+                error!("failed to training, error:{}", err);
+            }
+        }
+    }));
+
+    loop {
+        sched.tick();
+        std::thread::sleep(std::time::Duration::from_millis(500));
+    }
+}
+
+fn training(config: &config::Config, mysql_cli: &DefaultClient) -> MyResult<()> {
     let mut x:Vec<Vec<f64>> = vec![];
     let mut y:Vec<f64> = vec![];
     let mut existing_model:Option<ForecastModel> = None;
@@ -75,28 +91,28 @@ fn training(mysql_cli: &DefaultClient) -> MyResult<()> {
 
         debug!("fetch rates. begin:{}, end:{}", begin, end);
 
-        let rates = mysql_cli.select_rates_for_training(tx, "USDJPY", Some(begin), Some(end))?;
+        let rates = mysql_cli.select_rates_for_training(tx, &config.currency_pair, Some(begin), Some(end))?;
         debug!("fetched rates count: {}", rates.len());
 
         for offset in 0..rates.len() {
-            let truth = rates.get(offset + test_data_size - 1 + 5);
+            let truth = rates.get(offset + config.forecast_input_size - 1 + config.forecast_offset_minutes);
             if truth.is_none() {
                 break;
             }
             y.push(truth.unwrap().rate);
 
             let mut data:Vec<f64> = vec![];
-            for index in offset..offset+test_data_size {
+            for index in offset..offset+config.forecast_input_size {
                 data.push(rates[index].rate.clone());
             }
             x.push(data);
         }
 
-        existing_model = mysql_cli.select_forecast_model(tx, "USDJPY", 0)?;
+        existing_model = mysql_cli.select_forecast_model(tx, &config.currency_pair, 0)?;
 
         Ok(())
     })?;
-    if x.len() < 10 {
+    if x.len() < config.training_data_required_count {
         warn!("training data is too little. skip training. count:{}", x.len());
         return Ok(());
     }
@@ -119,7 +135,7 @@ fn training(mysql_cli: &DefaultClient) -> MyResult<()> {
         best_mne = Some(mne);
     }
 
-    for _ in 1..10 {
+    for _ in 1..config.training_count {
         let (x_train, x_test, y_train, y_test) = train_test_split(&matrix, &y, 0.2, true);
 
         let model = RandomForestRegressor::fit(&x_train, &y_train, Default::default())?;
@@ -150,7 +166,7 @@ fn training(mysql_cli: &DefaultClient) -> MyResult<()> {
 
     mysql_cli.with_transaction(|tx| {
         let bin = bincode::serialize(&best_model)?;
-        let m = ForecastModel::new("USDJPY".to_string(), 0, bin, "test".to_string())?;
+        let m = ForecastModel::new(config.currency_pair.clone(), config.forecast_model_no, bin, "test".to_string())?;
         mysql_cli.upsert_forecast_model(tx, &m)?;
         Ok(())
     })?;
